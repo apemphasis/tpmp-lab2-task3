c955358b5b837742b663980070e138497c0649fa
Yauheni Kazlouski <eu.new.gene@gmail.com>
2026-03-25 15:17:46 +0300

    add cli menu option

--- src/main.c
+++ src/main.c
@@ -25,6 +25,7 @@ int main(void) {
         printf("1. Создать массив маршрутов\n");
         printf("2. Вывести все маршруты\n");
         printf("3. Найти маршрут с максимальной длиной\n");
+        printf("4. Отсортировать маршруты по номерам\n");
         printf("0. Выход\n");
         printf("Выберите действие: ");
         scanf("%d", &choice);
@@ -60,6 +61,15 @@ int main(void) {
                     printf("Начальный пункт: %s\n", routes[max_index].start);
                     printf("Конечный пункт:  %s\n", routes[max_index].end);
                     printf("Длина:         %.2f\n", routes[max_index].distance);
+                }
+                break;
+
+            case 4:
+                if (current_size <= 0) {
+                    printf("Массив маршрутов пуст. Сначала создайте маршруты.\n");
+                } else {
+                    sort_by_number(routes, current_size);
+                    printf("Маршруты отсортированы по возрастанию номеров.\n");
                 }
                 break;
 
